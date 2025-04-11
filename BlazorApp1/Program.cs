25d5a9bb365d96c38676068e156edac213b94e50
sky-coop <3470791403@qq.com>
2025-04-11 14:43:56 +0800

    http

--- BlazorApp1/Program.cs
+++ BlazorApp1/Program.cs
@@ -2,14 +2,29 @@
 using BlazorApp1.Components;
 using System.Net;
 using System.Net.Sockets;
+using System.Security.Cryptography.X509Certificates;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// 获取本机IP地址
-string localIPAddress = GetLocalIPAddress();
-string url = $"https://{localIPAddress}:5000"; // 你可以修改端口号
-// 监听本机IP地址并配置端口
-builder.WebHost.UseUrls(url);
+//var cert = new X509Certificate2(
+//    Path.Combine(AppContext.BaseDirectory, "Input/FtpCert1.pfx"),
+//    "CertFtp");
+//string localIPAddress = GetLocalIPAddress();
+//string url = $"https://{localIPAddress}:5000"; // 你可以修改端口号
+//// 监听本机IP地址并配置端口
+//builder.WebHost.UseUrls(url);
+//// ✅ 配置 HTTPS 使用自定义证书
+//builder.WebHost.ConfigureKestrel(options =>
+//{
+//    options.Listen(IPAddress.Parse(localIPAddress), 5000, listenOptions =>
+//    {
+//        listenOptions.UseHttps(cert);
+//    });
+//});
+
+// 从 Render 环境变量获取端口（否则默认为 5000）
+var port = Environment.GetEnvironmentVariable("PORT") ?? "5000";
+builder.WebHost.UseUrls($"http://0.0.0.0:{port}");
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
